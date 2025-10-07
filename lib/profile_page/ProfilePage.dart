f70b4d9447743b5b212e94f6b775a6e839628c4f
Vandy Rizky Septiawan <vandyrizky33@gmail.com>
2025-10-07 13:59:14 +0800

    feat: menambahkan floating button

--- lib/profile_page/ProfilePage.dart
+++ lib/profile_page/ProfilePage.dart
@@ -4,6 +4,8 @@ import 'package:profile_page/profile_page/components/ProfileContainer.dart';
 import 'package:profile_page/profile_page/components/SocialSection.dart';
 
 class ProfilePage extends StatelessWidget {
+  final ScrollController _scrollController = ScrollController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -11,9 +13,20 @@ class ProfilePage extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.all(10),
         child: SingleChildScrollView(
+          controller: _scrollController,
           child: ProfileContainer(),
         )
       ),
+      floatingActionButton: FloatingActionButton(
+          onPressed: (){
+            _scrollController.animateTo(
+                0,
+                duration: const Duration(milliseconds: 500),
+                curve: Curves.easeInOut);
+          },
+        child: const Icon(Icons.arrow_upward, color: Colors.white,),
+        backgroundColor: Colors.black12,
+          ),
     );
   }
 }
